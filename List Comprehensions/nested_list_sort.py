3aed841793b09ba077f71b218103505f84a4838b
Anshukutty Abraham <85031368+anshukutty-abraham@users.noreply.github.com>
2022-11-15 17:59:38 +0100

    Update nested_list_sort.py

--- List Comprehensions/nested_list_sort.py
+++ List Comprehensions/nested_list_sort.py
@@ -1,1 +1,15 @@
-
+if __name__ == '__main__':
+    records=[]
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name,score])
+    records.sort()    
+    records1=dict(records)
+    val_list=sorted(set(records1.values()))
+    #print(val_list)
+    second_lowest_score=val_list[1]
+    #print(second_lowest_score)
+    for key,val in records1.items():
+        if val==second_lowest_score:
+            print(key)
